a131fe3fa2e11aa647a3fe8485e6fa6981dd5583
3ngel <angelina_panfilova@bk.ru>
2024-04-30 18:22:01 +0500

    I push it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule, RouterLinkActive } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PersonalPageComponent } from './personal-page/personal-page.component';
+// import { AppModule } from './app.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CommonModule, RouterLinkActive, HomePageComponent, PersonalPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PersonalPageComponent } from './personal-page/personal-page.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomePageComponent},
+    // {path: '**', redirectTo: 'home'},
+    {path: 'home', component: HomePageComponent},
+    {path: 'guide', component: PersonalPageComponent}
+];
+@NgModule({
+    imports:[
+        RouterModule.forRoot(routes),
+    ],
+    exports:[RouterModule],
+})
+export class AppRoutingModule{}
